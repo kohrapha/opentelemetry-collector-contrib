27d0f2f14f4153bc527c7b1ac516b755cb176e02
Raphael Koh <kohrapha@amazon.com>
2020-10-06 17:38:54 -0400

    Create MetricDeclaration struct

--- exporter/awsemfexporter/config_test.go
+++ exporter/awsemfexporter/config_test.go
@@ -57,5 +57,6 @@ func TestLoadConfig(t *testing.T) {
 			Region:                "us-west-2",
 			RoleARN:               "arn:aws:iam::123456789:role/monitoring-EKS-NodeInstanceRole",
 			DimensionRollupOption: "ZeroAndSingleDimensionRollup",
+			MetricDeclarations:    []*MetricDeclaration{},
 		})
 }
